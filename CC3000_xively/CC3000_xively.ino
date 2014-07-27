3df1abcc3d602515767f6e6478b9617b8a4c579b
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-07-27 09:14:02 +0200

    stability fix using watchdog

--- CC3000_xively/CC3000_xively.ino
+++ CC3000_xively/CC3000_xively.ino
@@ -8,10 +8,8 @@
 #include <Adafruit_CC3000.h>
 #include <ccspi.h>
 #include <SPI.h>
-#include <string.h>
-#include "utility/debug.h"
 #include "DHT.h"
-#include<stdlib.h>
+#include <avr/wdt.h>
 
 // Define CC3000 chip pins
 #define ADAFRUIT_CC3000_IRQ   3
@@ -30,13 +28,13 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
 DHT dht(DHTPIN, DHTTYPE);
 
 // WLAN parameters
-#define WLAN_SSID       "yourSSID"
-#define WLAN_PASS       "yourPassword"
+#define WLAN_SSID       "yourWiFiSSID"
+#define WLAN_PASS       "yourWiFiPassword"
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
 // Xively parameters
-#define WEBSITE  "https://api.xively.com"
-#define API_key  "yourAPIkey"
+#define WEBSITE  "api.xively.com"
+#define API_key  "yourAPIKey"
 #define feedID  "yourFeedID"
 
 uint32_t ip;
@@ -46,7 +44,7 @@ void setup(void)
   // Initialize
   Serial.begin(115200);
   
-  Serial.println(F("\nInitializing..."));
+  Serial.println(F("Initializing WiFi chip..."));
   if (!cc3000.begin())
   {
     Serial.println(F("Couldn't begin()! Check your wiring?"));
@@ -59,18 +57,20 @@ void loop(void)
 {
   // Connect to WiFi network
   cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY);
-  Serial.println(F("Connected!"));
+  Serial.println(F("Connected to WiFi network!"));
   
   /* Wait for DHCP to complete */
   Serial.println(F("Request DHCP"));
   while (!cc3000.checkDHCP())
   {
     delay(100);
-  }  
+  }
+  
+  // Start watchdog 
+  wdt_enable(WDTO_8S);  
 
   // Set the website IP
   uint32_t ip = cc3000.IP2U32(216,52,233,120);
-  cc3000.printIPdotsRev(ip);
   
   // Get data & transform to integers
   float h = dht.readHumidity();
@@ -82,58 +82,90 @@ void loop(void)
   // Prepare JSON for Xively & get length
   int length = 0;
 
-  String data = "";
-  data = data + "\n" + "{\"version\":\"1.0.0\",\"datastreams\" : [ {\"id\" : \"Temperature\",\"current_value\" : \"" + String(temperature) + "\"}," 
-  + "{\"id\" : \"Humidity\",\"current_value\" : \"" + String(humidity) + "\"}]}";
+  // JSON beginning
+  String data_start = "";
+  data_start = data_start + "\n" + "{\"version\":\"1.0.0\",\"datastreams\" : [ ";
   
-  length = data.length();
-  Serial.print("Data length");
-  Serial.println(length);
-  Serial.println();
+  // JSON for temperature & humidity
+  String data_temperature = "{\"id\" : \"Temperature\",\"current_value\" : \"" + String(temperature) + "\"},";
+  String data_humidity = "{\"id\" : \"Humidity\",\"current_value\" : \"" + String(humidity) + "\"}]}";
   
-  // Print request for debug purposes
-  Serial.print("PUT /v2/feeds/");
-  Serial.print(feedID);
-  Serial.println(".json HTTP/1.0");
-  Serial.println("Host: api.xively.com");
-  Serial.print("X-ApiKey: ");
-  Serial.println(API_key);
-  Serial.print("Content-Length: ");
-  Serial.println(length, DEC);
-  Serial.print("Connection: close");
-  Serial.println();
-  Serial.print(data);
-  Serial.println();
-  
+  // Get length
+  length = data_start.length() + data_temperature.length() + data_humidity.length();
+ 
+  // Reset watchdog
+  wdt_reset();
+ 
   // Send request
   Adafruit_CC3000_Client client = cc3000.connectTCP(ip, 80);
   if (client.connected()) {
-    Serial.println("Connected!");
-    client.println("PUT /v2/feeds/" + String(feedID) + ".json HTTP/1.0");
-    client.println("Host: api.xively.com");
-    client.println("X-ApiKey: " + String(API_key));
-    client.println("Content-Length: " + String(length));
-    client.print("Connection: close");
-    client.println();
-    client.print(data);
-    client.println();
+    Serial.println(F("Connected to Xively server."));
+    
+    // Send headers
+    Serial.print(F("Sending headers"));
+    client.fastrprint(F("PUT /v2/feeds/"));
+    client.fastrprint(feedID);
+    client.fastrprintln(F(".json HTTP/1.0"));
+    Serial.print(F("."));
+    client.fastrprintln(F("Host: api.xively.com"));
+    Serial.print(F("."));
+    client.fastrprint(F("X-ApiKey: "));
+    client.fastrprintln(API_key);
+    Serial.print(F("."));
+    client.fastrprint(F("Content-Length: "));
+    client.println(length);
+    Serial.print(F("."));
+    client.fastrprint(F("Connection: close"));
+    Serial.println(F(" done."));
+    
+    // Reset watchdog
+    wdt_reset();
+    
+    // Send data
+    Serial.print(F("Sending data"));
+    client.fastrprintln(F(""));    
+    client.print(data_start);
+    Serial.print(F("."));
+    wdt_reset();
+    client.print(data_temperature);
+    Serial.print(F("."));
+    wdt_reset();
+    client.print(data_humidity);
+    Serial.print(F("."));  
+    client.fastrprintln(F(""));
+    Serial.println(F(" done."));
+    
+    // Reset watchdog
+    wdt_reset();
+    
   } else {
     Serial.println(F("Connection failed"));    
     return;
   }
   
-  Serial.println(F("-------------------------------------"));
+  // Reset watchdog
+  wdt_reset();
+  
+  Serial.println(F("Reading answer..."));
   while (client.connected()) {
     while (client.available()) {
       char c = client.read();
       Serial.print(c);
     }
   }
-  client.close();
-  Serial.println(F("-------------------------------------"));
   
-  Serial.println(F("\n\nDisconnecting"));
+  // Reset watchdog
+  wdt_reset();
+   
+  // Close connection and disconnect
+  client.close();
+  Serial.println(F("Disconnecting"));
+  Serial.println(F(""));
   cc3000.disconnect();
+  
+  // Reset watchdog & disable
+  wdt_reset();
+  wdt_disable();
   
   // Wait 10 seconds until next update
   delay(10000);
